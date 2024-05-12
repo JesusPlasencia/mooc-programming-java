aa9ac5a670e5000523e884ce8752ba56a1e9b130
JesusPlasencia <jesusplasencia2508@hotmail.com>
2024-05-12 17:04:29 -0500

    Part 05: Abstraction

--- Part-05/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ Part-05/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,19 +3,69 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
-        Counter counter = new Counter();
-        System.out.println(counter.value());
-        counter.increase();
-        counter.increase();
-        counter.increase();
-        counter.increase(10);
-        
-        counter.decrease();
-        counter.decrease();
-        counter.decrease();
-        counter.decrease();
-        counter.decrease(6);
-        
-        System.out.println(counter.value());
+        int value = 10;
+        System.out.println(value);
+
+        // Class Name
+        Person joan = new Person("Joan Ball");
+        System.out.println(joan);
+
+        joan = null;
+        joan.growOlder();
+        // toString function is to access to the value of the variable
+    }
+}
+
+class Person {
+
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+
+    public Person(String name) {
+        this(name, 0, 0, 0);
+    }
+
+    public Person(String name, int age, int height, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    // other constructors and methods
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void growOlder() {
+        this.age = this.age + 1;
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heightPerHundred = this.height / 100.0;
+        return this.weight / (heightPerHundred * heightPerHundred);
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
     }
 }
